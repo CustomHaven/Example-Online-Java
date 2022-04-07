88eacf833001169745647dd1f86beed4e4831d26
Mohamed <mmuseaden@gmail.com>
2022-04-07 15:02:40 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+
+        Person personA = new Person("Matt",23 );
+        Person personB = new Person("Meg", 22 );
+
+        personB.Introduce();
+        personA.Introduce();
+    }
+}
